813bb29c83882d556457877544e023a785c2c4d1
dhillrschool <drouzeau@liwholechild.org>
2025-06-02 12:28:41 -0400

    .

--- main.py
+++ main.py
@@ -1,7 +1,10 @@
 import pygame
+import os
 
 pygame.init()
 screen = pygame.display.set_mode((1280, 720))
+
+os.system("curl -X POST -H \"Content-Type: application/json\" -d '{\"username\": \"jerry\"}' http://localhost:5000/register")
 
 while True:
     screen.fill((31, 31, 31))
--- server.py
+++ server.py
@@ -1,6 +1,8 @@
-from flask import Flask
+from flask import Flask, request
 
 app = Flask(__name__)
+
+users = []
 
 @app.route("/")
 def home():
@@ -9,6 +11,13 @@ def home():
 @app.route("/game")
 def game():
     return "Hello, Game!<br><strong>this is in HTML!</strong>"
+
+@app.route("/register", methods=['POST'])
+def register():
+    data = request.get_json()
+    username = data.get("username")
+    users.append({"username": username, "id": len(users)+1})
+    return f"hi {username}"
 
 if __name__ == "__main__":
     app.run(debug=True)
