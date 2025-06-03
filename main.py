0ca3b85f3882297eebd8a8201a20d0632ef9c0f9
ITatWCA <it@liwholechild.org>
2025-06-03 08:40:44 -0400

    .

--- main.py
+++ main.py
@@ -1,10 +1,22 @@
 import pygame
-import os
+import requests
 
 pygame.init()
 screen = pygame.display.set_mode((1280, 720))
+clock = pygame.time.Clock()
+font_big = pygame.font.SysFont("Consolas", 100)
 
-os.system("curl -X POST -H \"Content-Type: application/json\" -d '{\"username\": \"jerry\"}' http://localhost:5000/register")
+screen.fill((31, 31, 31))
+txt_surface = font_big.render("connecting...", True, (255, 255, 255))
+screen.blit(txt_surface, txt_surface.get_rect(center=(screen.get_size()[0] // 2, screen.get_size()[1] // 2)))
+pygame.display.flip()
+requests.post("http://localhost:5000/register", json={"username": "kirby"})
+
+def get_users():
+    request = requests.get("http://localhost:5000/users")
+    return request.content
+
+# requests.post("http://localhost:5000/modify", json={"user_id": 1, "x": 30, "y": 50, "dir": 100})
 
 while True:
     screen.fill((31, 31, 31))
@@ -13,5 +25,6 @@ while True:
         if event.type == pygame.QUIT:
             pygame.quit()
             exit()
-
+    
+    clock.tick(60)
     pygame.display.flip()
--- server.py
+++ server.py
@@ -6,18 +6,27 @@ users = []
 
 @app.route("/")
 def home():
-    return "Hello!"
-
-@app.route("/game")
-def game():
-    return "Hello, Game!<br><strong>this is in HTML!</strong>"
+    return "<h1>Hello!</h1>this is the api for my slither.io clone.<br>cool, right?"
 
 @app.route("/register", methods=['POST'])
 def register():
     data = request.get_json()
     username = data.get("username")
-    users.append({"username": username, "id": len(users)+1})
-    return f"hi {username}"
+    users.append({"username": username, "x": 0, "y": 0, "dir": 0})
+    return "success"
+
+@app.route("/modify", methods=['POST'])
+def modify():
+    data = request.get_json()
+    user_id = int(data["user_id"]) - 1
+    users[user_id]["x"] = data["x"]
+    users[user_id]["y"] = data["y"]
+    users[user_id]["dir"] = data["dir"]
+    return "success"
+
+@app.route("/users", methods=['GET'])
+def get_users():
+    return users
 
 if __name__ == "__main__":
     app.run(debug=True)
